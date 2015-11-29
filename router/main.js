fa5542b8f3f933432d01ba795781c40f1f324eb4
CameronRP <cameron.ryan.pears@gmail.com>
2015-11-29 17:35:38 +1300

    removed deprecated upload post URL

--- router/main.js
+++ router/main.js
@@ -1,7 +1,5 @@
-var util = require('../util'),
-	dataValidation = require('../dataValidation'),
-	orm = require('../models/orm'),
-	log = require('../logging');
+var	orm = require('../models/orm');
+var	log = require('../logging');
 
 module.exports = function(app) {
 
@@ -14,29 +12,4 @@ module.exports = function(app) {
     response.render('upload.jade')
   });
 
-  app.post('/upload',function(request, response) {
-   	log.debug("Request handler 'upload' was called.");
-   	log.info('Upload request started.');
-
-   	dataValidation.validUploadRequest(request)
-   	.then(util.registerDeviceIfNotAlready)
-   	.then(dataValidation.dataIDs)
-   	//.then(util.uploadFile)
-   	.then(util.uploadDataPoint)
-   	.then(function(dataPoint){
-   		log.debug("Responding to device.");
-   		response.writeHead(200, {"Content-Type": "text/html"});
-   		var responseBody = util.generateSuccessResponse(dataPoint)
-   		response.write(responseBody);
-   		response.end();
-   		log.debug("Responded back to device.");
-   		log.verbose('Responded to device with:', responseBody);
-      util.uploadFile(dataPoint);
-   	}).catch(function(err) {
-   		log.error('Error with upload:', err);
-   		response.writeHead(400, {"Content-Type": "text/html"});
-   		response.write('Error with processing request.');
-   		response.end();
-   	});
-  });
 }
