6891f5c1ba1bfb32ccbc04ed1e321a519e0b92d3
CameronRP <cameron.ryan.pears@gmail.com>
2016-02-10 14:21:02 +1300

    Added API Documentation.

--- router/apiv1.js
+++ router/apiv1.js
@@ -9,10 +9,37 @@ var baseUrl = '/api/v1';
 
 module.exports = function(app) {
 
+	/**
+	 * @api {post} /api/v1/audioRecordings Add AudioRecording
+	 * @apiName PostAudioRecording
+	 * @apiGroup AudioRecording
+	 * @apiVersion 1.0.0
+	 *
+	 * @apiParam {JSON} data			 Metadata for the AudioRecording.
+	 * @apiParam {File} recording  The audio file.
+	 *
+	 * @apiParamExample {json} Data-Example:
+	 *		{
+	 *			"audioFile": { "startTimestamp": "2016-01-1 12:30:20.123+1300", "duration": 120},
+	 *			"device": { "id": 123, "type": "cacophonometer1.0"},
+	 *			"recordingRule": { "id": 123, "name": "RuleName", "duration": 120},
+	 *			"location": { "id": 123, "latitude": 123123123, "longitude": 321321321, "timestamp": "2016-01-1 12:30:20.123+1300", "accuracy": 40},
+	 *			"hardware": { "id": 123, "manufacturer": "CacoMan", "model": "M-Two", "solarPanelPower": 6000},
+	 *			"software": { "id": 123, "version": 0.1.2},
+	 *			"microphone": { "id": 123, "type": "electret" },
+	 *			"environment": { "id": 123, "tempreature": 21},
+	 *			"batteryPercentage": 48,
+	 *			"tags": {},
+	 *			"extra": {}
+ 	 *		}
+	 */
+
 	app.post(baseUrl+'/audioRecordings', function(req, res) {
 		var audioRecording;
 		var form = new formidable.IncomingForm();
 		form.parse(req, function(err, fields, files) {
+			console.log(fields);
+			console.log(files);
 			var data = JSON.parse(fields.data);
 			if (!data.audioFile) {
 				log.warn('No field "audioFile" in uploaded data.');
@@ -31,6 +58,16 @@ module.exports = function(app) {
 		  });
 		});
 	});
+
+	/**
+	 * @api {post} /api/v1/videoRecordings Add VideoRecording
+	 * @apiName PostVideoRecording
+	 * @apiGroup VideoRecording
+	 * @apiVersion 1.0.0
+	 *
+	 * @apiParam {JSON} data			 Metadata for the VideoRecording.
+	 * @apiParam {File} recording  The audio file.
+	 */
 
 	app.post(baseUrl+'/videoRecordings', function(req, res) {
 		var audioRecording;
