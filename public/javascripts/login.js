fa83ddde17db9011b5f67e1a5561e09da6a5ae40
CameronRP <cameron.ryan.pears@gmail.com>
2017-03-22 12:52:07 +1300

    Updated login page.

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -3,22 +3,29 @@ window.onload = function() {
 };
 
 function login() {
+  var password = document.getElementById('inputPassword').value;
+  var username = document.getElementById('inputUsername').value;
+
   $.ajax({
     url: '/authenticate_user',
     type: 'post',
-    data: "password="+password.value+"&username="+username.value,
+    data: "password="+password+"&username="+username,
     success: loginSuccess,
     error: loginError
   });
 }
 
 function loginSuccess(res) {
-  console.log("login");
   sessionStorage.setItem('token', res.token);
   sessionStorage.setItem('userData', JSON.stringify(res.userData));
   window.location.assign("/user_home");
 }
 
 function loginError(res) {
-  console.log("Error");
+  document.getElementById('inputUsername').value = '';
+  document.getElementById('inputPassword').value = '';
+  var messages = JSON.parse(res.responseText).messages.join('.');
+  console.log(messages);
+  window.alert(messages);
+  document.getElementById('messages').innerHTML = messages;
 }
