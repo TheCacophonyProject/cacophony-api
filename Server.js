93b12781d9e3dd085a1ae53fb5454a38cd83dd24
Cameron <cameron.ryan.pears@gmail.com>
2017-09-07 15:28:18 +1200

    Added 'where', 'offset', 'limit', and 'Authorization' to the allowed headers in CORS requests. This allows the Web Interface to do API calls with these headers. https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Headers

--- Server.js
+++ Server.js
@@ -35,7 +35,7 @@ log.addExpressApp(app);
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'where, offset, limit, Authorization');
   next();
 });
 
