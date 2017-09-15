98319d265e6f1648958fbcb46f36b22d93b6ed19
Menno Finlay-Smits <inbox@menno.io>
2017-09-15 12:03:08 +1200

    Be explicit about success value

--- api/V1/apidoc.js
+++ api/V1/apidoc.js
@@ -1,12 +1,12 @@
 /**
  * @apiDefine V1ResponseSuccess
- * @apiSuccess {Boolean} success If the request was successful.
+ * @apiSuccess {Boolean} success `true` - Request was successful.
  * @apiSuccess {String[]} messages Messages about the request.
  */
 
 /**
  * @apiDefine V1ResponseError
- * @apiError {Boolean} success If the request was successful.
+ * @apiError {Boolean} success `false` - Request failed.
  * @apiError {String[]} messages Messages about the error.
  */
 
