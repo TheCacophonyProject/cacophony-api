51abee720a1a4a53e829eacf8ad0ebe15f255785
Clare McLennan <clare@Clares-MacBook-Air-2.local>
2018-03-16 18:44:01 +1300

    Added the templat file too..

---
+++ config/app_TEMPLATE.js
@@ -0,0 +1,44 @@
+// Config instructions: Fill out required fields and save as 'config.js'
+
+// ======= SERVER ==========
+server = { // General server settings
+  passportSecret: null, // REQUIRED, String. Random string used for passport module for encrypting JWT.
+  loggerLevel: "debug", // REQUIRED, one of ('debug', 'warning', 'info', 'error')
+  http: {
+    active: true,
+    port: 80,
+  },
+};
+
+// ======= S3 compatible object store settings, e.g. Minio ===========
+s3 = {  // Used for storing audio & video recordings.
+  publicKey: null,  // REQUIRED, String (aka Minio AccessKey)
+  privateKey: null, // REQUIRED, String (aka Minio SecretKey)
+  bucket: "cacophony",   // REQUIRED, String  (must exist in the store)
+  endpoint: "http://localhost:9000", // REQUIRED, URL
+};
+
+// ======= Logging =======
+logging = {
+  folder: "logFiles", // REQUIRED Folder of the logging file.
+}
+
+// ======= File Processing =======
+fileProcessing = {
+  port: 2002,
+};
+
+// ======= Database settings =======
+database = {
+  username: "root",
+  password: null,
+  database: "cacophony",
+  host: "localhost",
+  dialect: "postgres"
+};
+
+exports.server = server;
+exports.s3 = s3;
+exports.logging = logging;
+exports.fileProcessing = fileProcessing;
+exports.database = database;
