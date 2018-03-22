3bb4fe37e27edd032211a6248c6b8b1c52949ab7
Menno Finlay-Smits <inbox@menno.io>
2018-03-23 10:51:13 +1300

    Send sequelize logs via winston & include query timings
    Sequelize's logs are logged at debug.

--- models/index.js
+++ models/index.js
@@ -2,17 +2,27 @@ var fs = require('fs');
 var path = require('path');
 var Sequelize = require('sequelize');
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || 'development';
 var config = require('../config');
+var log = require('../logging');
 
 var db = {};
 
 var dbConfig = config.database;
+
+// Have sequelize send us query execution timings
+dbConfig.benchmark = true;
+
+// Send logs via winston
+dbConfig.logging = function(msg, timeMs) {
+  log.debug("%s [%d ms]", msg, timeMs);
+};
+
 var sequelize = new Sequelize(
   dbConfig.database,
   dbConfig.username,
   dbConfig.password,
-  dbConfig);
+  dbConfig,
+);
 
 fs
   .readdirSync(__dirname)
