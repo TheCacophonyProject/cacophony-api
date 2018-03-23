925dec0f6cb3215b3f10a0374a967da9bf62d50b
Menno Finlay-Smits <inbox@menno.io>
2018-03-23 14:59:37 +1300

    Add initial tests for AudioRecordings API
    These tests that audio can be uploaded and is visible on the
    server. More to come.

--- test/testuser.py
+++ test/testuser.py
@@ -1,30 +1,36 @@
 from testexception import TestException
 
-class TestUser:
 
+class TestUser:
     def __init__(self, username, userapi):
         self._userapi = userapi
         self.username = username
 
+    def can_see_audio_recording_from_group(self, groupName):
+        recordings = self._userapi.query_audio(limit=1)
+        assert recordings, \
+            "User '{}' could not see any recordings.".format(self.username)
+
+        lastGroup = recordings[0]['group']
+        assert lastGroup == groupName, \
+            "Latest audio recording is from group '{}', not from '{}'".format(lastGroup, groupName)
 
     def can_see_recording_from(self, testdevice):
         recordings = self._userapi.query(limit=1)
-        if not recordings:
-            raise TestException("User '{}' could not see any recordings.".format(self.username))
+        assert recordings, \
+            "User '{}' could not see any recordings.".format(self.username)
 
         lastDevice = recordings[0]['Device']['devicename']
-        if (lastDevice != testdevice.devicename):
-            raise TestException("Lastest recording was not from device '{}', not from '{}'".format(lastDevice, testdevice.devicename))
-            
+        assert lastDevice == testdevice.devicename, \
+            "Latest recording is from device '{}', not from '{}'".format(lastDevice, testdevice.devicename)
+
     def cannot_see_any_recordings(self):
         recordings = self._userapi.query(limit=1)
-        if recordings:
-            raise TestException("User '{}' can see a recording from '{}'".format(self.username, recordings[0]['Device']['devicename']))
-        
+        assert not recordings, \
+            "User '{}' can see a recording from '{}'".format(self.username, recordings[0]['Device']['devicename'])
 
     def create_group(self, groupname):
         self._userapi.create_group(groupname)
 
     def get_user_details(self, user):
         self._userapi.get_user_details(user.username)
-        
