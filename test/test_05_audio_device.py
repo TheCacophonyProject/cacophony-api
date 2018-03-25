4596188fb7404002b40cc4e459c5dc8c5f95c38f
Menno Finlay-Smits <inbox@menno.io>
2018-03-26 10:52:44 +1300

    Split up audio vs thermal device tests

---
+++ test/test_05_audio_device.py
@@ -0,0 +1,19 @@
+from helper import Helper
+
+
+class TestAudioDevice:
+    def test_can_upload_audio(self):
+        helper = Helper()
+
+        description = "If a new device 'Listener' signs up"
+        listener = helper.given_new_device(
+            self, 'Listener', description=description)
+
+        print("Then 'Listener' should able to log in")
+        helper.login_as_device(listener.devicename)
+
+        print("And 'Listener' should be able to upload an audio file")
+        recording = listener.upload_audio_recording()
+
+        print("And the audio file should be visible to super users")
+        helper.admin_user().can_see_audio_recording(recording.recordingId)
