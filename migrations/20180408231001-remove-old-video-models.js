07641f247bcca95941b73ccd09cf638a2da438b6
Menno Finlay-Smits <inbox@menno.io>
2018-04-09 11:27:19 +1200

    Remove old IR and thermal recording models

---
+++ migrations/20180408231001-remove-old-video-models.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: async function (queryInterface) {
+    // Disconnect ThermalVideoRecordings and IrVideoRecordings from other tables.
+    await queryInterface.removeColumn('Tags', 'IrVideoRecordingId');
+    await queryInterface.removeColumn('Tags', 'ThermalVideoRecordingId');
+
+    await queryInterface.dropTable('ThermalVideoRecordings');
+    return queryInterface.dropTable('IrVideoRecordings');
+  },
+
+  down: async function () {
+    // No going back.
+  }
+};
