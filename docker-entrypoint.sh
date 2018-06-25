5b34c74d36ff31b8a6ee7877d227bfc96ea94844
Menno Finlay-Smits <inbox@menno.io>
2018-06-25 17:47:05 +1200

    Move minio startup to docker-entrypoint
    This makes more sense given that setting up Minio doesn't depend on
    the current code checkout. Moving this speeds container startup up
    somewhat (e.g. no need for 10s wait).

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e
 
+export MINIO_ACCESS_KEY=minio
+export MINIO_SECRET_KEY=miniostorage
+./minio server --address :9001 .data &> minio.log &
+
 service postgresql start
 
 sudo -i -u postgres psql -c "CREATE USER test with password 'test'"
 sudo -i -u postgres psql -c "CREATE DATABASE cacophonytest WITH OWNER test;"
 sudo -i -u postgres psql cacophonytest -c "CREATE EXTENSION postgis"
+
+./mc config host add myminio http://127.0.0.1:9001 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+./mc mb myminio/cacophony
 
 echo "[hit enter key to exit] or run 'docker stop <container>'"
 
--- docker-start.sh
+++ docker-start.sh
@@ -3,7 +3,7 @@
 image_name=cacophony-api
 container_name="cacophony-api-test"
 
-echo "Stopping $cacophony-api-test container"
+echo "Stopping $container_name container (if running)"
 sudo docker rm --force $container_name &> /dev/null
 
 sudo docker build . -t $image_name
@@ -22,9 +22,4 @@ sudo docker exec $container_name bash -c "$@ rm -r /node_modules"
 sudo docker exec $container_name bash -c "$@ echo 'npm install...'        && npm install"
 sudo docker exec $container_name bash -c "$@ echo 'migrating database...' && node_modules/sequelize-cli/bin/sequelize db:migrate --config config/app_test_default.js"
 sudo docker exec $container_name bash -c "sudo -i -u postgres psql cacophonytest -f /test/db-seed.sql"
-sudo docker exec $container_name bash -c "$@ MINIO_ACCESS_KEY=minio MINIO_SECRET_KEY=miniostorage ./minio server --address :9001 .data &> miniolog &"
-sudo docker exec $container_name bash -c "$@ sleep 10" # Allowing minio to initialize
-sudo docker exec $container_name bash -c "./mc config host add myminio http://127.0.0.1:9001 minio miniostorage"
-sudo docker exec $container_name bash -c "./mc mb myminio/cacophony"
-
 sudo docker exec $container_name bash -c "$@ node /Server.js --config=config/app_test_default.js${BG:+ &> /dev/null &}"
