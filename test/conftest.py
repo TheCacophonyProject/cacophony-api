d62f0281f4d9d4555c42ca7993413d425328ffec
Menno Finlay-Smits <inbox@menno.io>
2018-11-30 11:03:22 +1300

    Add file procesing API URL to test config
    ... and clean up config handling code a little.

--- test/conftest.py
+++ test/conftest.py
@@ -6,8 +6,20 @@ made available for use within tests.
 import pytest
 
 from .helper import Helper
+from .fileprocessingapi import FileProcessingAPI
+from .testconfig import TestConfig
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
+def test_config():
+    return TestConfig().load_config()
+
+
+@pytest.fixture(scope="module")
 def helper():
     return Helper()
+
+
+@pytest.fixture(scope="module")
+def file_processing(test_config):
+    return FileProcessingAPI(test_config.fileprocessing_url)
