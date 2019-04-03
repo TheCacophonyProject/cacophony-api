f6e8d642d3aa60a8560d5c40cb5c7519c9472a26
Menno Finlay-Smits <inbox@menno.io>
2019-04-03 23:41:06 +1300

    Build releases in TravisCI
    TravisCI will now build Debian release packages for tagged revisions
    and upload them to Github Releases.

---
+++ _release/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+fatal() {
+    echo "$1"
+    exit 1
+}
+
+version=${1/v/}
+if [[ "$version" == "" ]]; then
+    fatal "usage: $0 <version>"
+fi
+
+which json || fatal "Please install the json tool ('npm install -g json')"
+
+set -e
+
+# Start from the root of the repo
+root=`git rev-parse --show-toplevel`
+cd $root
+
+echo "Setting up build directory..."
+build_dir=${root}/dist/build
+rm -rf ${build_dir}
+mkdir -p ${build_dir}
+
+echo "Extracting source tree..."
+git archive HEAD | tar -x -C ${build_dir}
+cp _release/* ${build_dir}/_release  # makes things easier while developing release process
+
+cd ${build_dir}
+
+echo "Setting versions..."
+perl -pi -e "s/^version:.+/version: \"${version}\"/" _release/nfpm.yaml
+json -I -f package.json -e "this.version=\"${version}\""
+json -I -f package-lock.json -e "this.version=\"${version}\""
+
+echo "Installing dependencies..."
+npm install
+
+echo "Creating API docs..."
+npm run apidoc
+
+nfpm -f _release/nfpm.yaml pkg -t ../cacophony-api_${version}.deb
