812a7a1aae7b2a68098381fe91a1545e317336e3
Giampaolo <g.ferraro22@gmail.com>
2019-06-17 11:13:30 +1200

    updated:

--- models/Recording.js
+++ models/Recording.js
@@ -32,8 +32,7 @@ module.exports = function(sequelize, DataTypes) {
 
   var attributes = {
     // recording metadata.
-    type: DataTypes.STRING,
-    duration: DataTypes.INTEGER,
+    type: DataTypes.STRING,duration: DataTypes.INTEGER,
     recordingDateTime: DataTypes.DATE,
     location: {
       type: DataTypes.GEOMETRY,
