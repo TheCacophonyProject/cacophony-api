361d3d2e7af8548d983b8d4c703885efd93d0519
Restyled.io <commits@restyled.io>
2019-06-19 01:49:19 +0000

    Restyled by prettier

--- api/customErrors.js
+++ api/customErrors.js
@@ -25,7 +25,8 @@ function errorHandler(err, request, response, next) {
     err = new ClientError(err.message, 422); // Convert invalid JSON body error to UnprocessableEntity
   }
   if (err instanceof CustomError) {
-    log.warn(err.toString());return response.status(err.statusCode).json(err.toJson());
+    log.warn(err.toString());
+    return response.status(err.statusCode).json(err.toJson());
   }
   log.error(err);
   response.status(500).json({
