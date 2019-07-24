b924de9153ddb96cad57415175434b80566c6d60
Cameron Ryan-Pears <cameron.ryan.pears@gmail.com>
2019-07-25 01:21:56 +1200

    Make groupname unique

---
+++ migrations/20190724130852-add-index-to-group.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+  up: function(queryInterface) {
+    return queryInterface.addIndex("Groups", {
+      fields: ["groupname"],
+      unique: true
+    });
+  },
+
+  down: function(queryInterface) {
+    return queryInterface.removeIndex("Groups", ["groupname"]);
+  }
+};
