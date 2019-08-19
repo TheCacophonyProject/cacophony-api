a29f1ecd083ce38934084d95abaef565e8d73c9a
Menno Finlay-Smits <inbox@menno.io>
2019-08-19 14:24:34 +1200

    Test devices with no recent audio lure events

--- test/test_report.py
+++ test/test_report.py
@@ -5,37 +5,43 @@ import dateutil.tz
 
 class TestReport:
     def test_report(self, helper):
-        device = helper.given_new_device(self)
         user = helper.admin_user()
-        now = datetime.now()
 
-        # Add some audio events
+        # Two devices
+        device0 = helper.given_new_device(self)
+        device1 = helper.given_new_device(self)
+
+        # Add some audio events for device0 (device1 won't have any)
         now = datetime.now(dateutil.tz.tzlocal()).replace(microsecond=0)
         file_id = user.upload_audio_bait({"name": "other-sound"})
-        device.record_event("audioBait", {"fileId": file_id}, [now - timedelta(minutes=5)])
+        device0.record_event("audioBait", {"fileId": file_id}, [now - timedelta(minutes=5)])
 
         exp_audio_bait_name = "some-sound"
         exp_audio_bait_time = now - timedelta(minutes=2)  # newer
         file_id = user.upload_audio_bait({"name": exp_audio_bait_name})
-        device.record_event("audioBait", {"fileId": file_id}, [exp_audio_bait_time])
+        device0.record_event("audioBait", {"fileId": file_id}, [exp_audio_bait_time])
 
-        # Add 2 recordings
-        rec1 = device.upload_recording()
-        rec2 = device.upload_recording()
+        # Add 2 recordings for device0
+        rec0 = device0.upload_recording()
+        rec1 = device0.upload_recording()
+
+        # Add a recording for device1
+        rec2 = device1.upload_recording()
 
         # Add recording tag to 1st recording.
-        rec1.is_tagged_as(what="cool").by(user)
+        rec0.is_tagged_as(what="cool").by(user)
 
-        # Add track and track tags to 2nd recording.
-        user.update_recording(rec2, comment="foo")
-        track = user.can_add_track_to_recording(rec2)
+        # Add track and track tags to rec1
+        user.update_recording(rec1, comment="foo")
+        track = user.can_add_track_to_recording(rec1)
         user.can_tag_track(track, what="possum", automatic=True)
         user.can_tag_track(track, what="rat", automatic=False)
         user.can_tag_track(track, what="stoat", automatic=False)
 
         report = ReportChecker(user.get_report(limit=10))
-        report.check_line(rec1, device, exp_audio_bait_name, exp_audio_bait_time)
-        report.check_line(rec2, device, exp_audio_bait_name, exp_audio_bait_time)
+        report.check_line(rec0, device0, exp_audio_bait_name, exp_audio_bait_time)
+        report.check_line(rec1, device0, exp_audio_bait_name, exp_audio_bait_time)
+        report.check_line(rec2, device1, None, None)
 
 
 class ReportChecker:
@@ -73,11 +79,12 @@ class ReportChecker:
         assert line["human_track_tags"] == format_tags(expected_human_tags)
         assert line["recording_tags"] == format_tags(t["what"] for t in rec.tags)
 
-        assert line["last_audio_bait"] == exp_audio_bait_name
-        assert_times_equiv(dateutil.parser.parse(line["last_audio_bait_time"]), exp_audio_bait_time)
-        assert float(line["mins_since_last_audio_bait"]) == round(
-            (recording_time - exp_audio_bait_time).total_seconds() / 60, 1
-        )
+        if exp_audio_bait_name:
+            assert line["last_audio_bait"] == exp_audio_bait_name
+            assert_times_equiv(dateutil.parser.parse(line["last_audio_bait_time"]), exp_audio_bait_time)
+            assert float(line["mins_since_last_audio_bait"]) == round(
+                (recording_time - exp_audio_bait_time).total_seconds() / 60, 1
+            )
 
         assert line["url"] == "http://test.site/recording/" + str(rec.id_)
 
