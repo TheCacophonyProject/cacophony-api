524f5ed48970e20cc26db8af2cdb7b84750affc7
Menno Finlay-Smits <inbox@menno.io>
2019-09-16 11:44:05 +1200

    Fixed sporadically failing device query tests
    The tests were relying on the order of API query results.

--- test/test_device_query.py
+++ test/test_device_query.py
@@ -31,7 +31,7 @@ class TestDeviceQuery:
 
         devices = clare.query_devices(groups=[gary_group])
         ids = set([device["id"] for device in devices])
-        assert ids == set()
+        assert not ids
 
         devices = gary.query_devices(devices=[terminator], groups=[gary_group, clares_group])
         ids = set([device["id"] for device in devices])
@@ -51,8 +51,8 @@ class TestDeviceQuery:
         access = {"devices": "r"}
         clare.new_token(access, set_token=True)
         devices = clare.query_devices(devices=[terminator, terminator2])
-        ids = [device["id"] for device in devices]
-        assert ids == [terminator.get_id(), terminator2.get_id()]
+        ids = set([device["id"] for device in devices])
+        assert ids == set([terminator.get_id(), terminator2.get_id()])
         new_group = helper.make_unique_group_name(self, "clares_group")
         with pytest.raises(AuthenticationError):
             clare.create_group(new_group)
@@ -60,7 +60,7 @@ class TestDeviceQuery:
         devices = clare.query_devices(
             devices=[terminator, terminator2], groups=[clares_group], operator="AND"
         )
-        ids = [device["id"] for device in devices]
-        assert ids == [terminator.get_id(), terminator2.get_id()]
+        ids = set(device["id"] for device in devices)
+        assert ids == set([terminator.get_id(), terminator2.get_id()])
         with pytest.raises(UnprocessableError):
             devices = clare.query_devices(devices=[terminator, terminator2], operator="in")
