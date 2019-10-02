30d76e17174db1f4494a95ad319424d5963082de
GP <g.ferraro22@gmail.com>
2019-10-03 12:39:04 +1300

    added migration (#269)

---
+++ migrations/20191002224957-rename-bad-tracks.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.sequelize.query(
+      `UPDATE "TrackTags" set "what"='poor tracking' where "what" = 'bad track'`
+    );
+    await queryInterface.sequelize.query(
+      `UPDATE "TrackTags" set "what"='unknown' where "what" = 'unidentified'`
+    ); 
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.sequelize.query(
+      `UPDATE "TrackTags" set "what"='bad track' where "what" = 'poor tracking'`
+    );
+    await queryInterface.sequelize.query(
+      `UPDATE "TrackTags" set "what"='unidentified' where "what" = 'unknown'`
+    );
+  }
+};
