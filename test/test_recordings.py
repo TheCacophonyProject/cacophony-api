a5b750b9800b60242e72d9d066a46f98b8a0325e
Jack Steel <mail@jacksteel.co.nz>
2020-01-07 00:48:48 +1100

    Add test to make sure fileKey(s) aren't returned

--- test/test_recordings.py
+++ test/test_recordings.py
@@ -18,3 +18,17 @@ class TestRecordings:
         assert "downloadRawJWT" in response
         print("  But the response should not have a JWT for the processed file")
         assert "downloadFileJWT" not in response
+
+    def test_recording_doesnt_include_file_key(self, helper):
+        print("If a new user uploads a recording")
+        bob = helper.given_new_user(self, "bob_limit")
+        bobsGroup = helper.make_unique_group_name(self, "bobs_group")
+        bob.create_group(bobsGroup)
+        bobsDevice = helper.given_new_device(self, "bobs_device", bobsGroup)
+        recording = bobsDevice.upload_recording()
+
+        print("And then fetches it")
+        recording_response = bob.get_recording(recording)
+
+        print("  The recording response should not contain the rawFileKey")
+        assert "rawFileKey" not in recording_response
