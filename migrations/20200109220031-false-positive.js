c71cb064c0280f7c1d8b5133f73e47b1745c40fe
Giampaolo <g.ferraro22@gmail.com>
2020-01-10 11:02:56 +1300

    added migration to rename false positives

---
+++ migrations/20200109220031-false-positive.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports = {
+  up: async queryInterface => {
+    await queryInterface.sequelize.query(
+      `UPDATE "TrackTags" set "what"='false-positive' where "what" = 'false positive'`
+    );
+  },
+
+  down: async queryInterface => {
+    await queryInterface.sequelize.query(
+      `UPDATE "TrackTags" set "what"='false positive' where "what" = 'false-positive'`
+    );
+  }
+};
