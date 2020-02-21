c8371e2b7e45b64b5b1aef195b3de0d21b75540a
Jon Hardie <jon@hardiesoft.com>
2020-02-22 12:59:02 +1300

    Add database migration to add an index to event dateTime field, to speed up reporting queries that generate CSV files.

---
+++ migrations/20200221232549-add-index-to-event-datetime.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: async queryInterface => {
+    await queryInterface.sequelize.query(
+      `CREATE INDEX events_datetime ON "Events" ("dateTime");`
+    );
+  },
+
+  down: async queryInterface => {
+    await queryInterface.sequelize.query(
+      `DROP INDEX events_datetime;`
+    );
+  }
+};
