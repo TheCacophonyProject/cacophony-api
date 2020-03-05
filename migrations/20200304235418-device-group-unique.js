c5d79a7efd99d97c30ea3d0c240a5a6f631b5016
Giampaolo <g.ferraro22@gmail.com>
2020-03-05 15:35:43 +1300

    updated beautify

--- migrations/20200304235418-device-group-unique.js
+++ migrations/20200304235418-device-group-unique.js
@@ -1,6 +1,6 @@
 "use strict";
 // CITEXT extension must be installed with a more privilaged user
-// CREATE EXTENSION IF NOT EXISTS citext;  
+// CREATE EXTENSION IF NOT EXISTS citext;
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -8,13 +8,13 @@ module.exports = {
 
     await queryInterface.changeColumn("Devices", "devicename", {
       type: Sequelize.CITEXT
-    });    
+    });
     await queryInterface.changeColumn("Groups", "groupname", {
       type: Sequelize.CITEXT
     });
-     await queryInterface.changeColumn("Users", "username", {
+    await queryInterface.changeColumn("Users", "username", {
       type: Sequelize.CITEXT
-    });       
+    });
     await queryInterface.changeColumn("Users", "email", {
       type: Sequelize.CITEXT
     });
@@ -23,25 +23,26 @@ module.exports = {
       type: "unique",
       name: "Devices_devicename_group_key"
     });
-
-
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeConstraint("Devices","Devices_devicename_group_key");
+    await queryInterface.removeConstraint(
+      "Devices",
+      "Devices_devicename_group_key"
+    );
     await queryInterface.addConstraint("Devices", ["devicename"], {
       type: "unique",
       name: "Devices_devicename_key"
     });
     await queryInterface.changeColumn("Devices", "devicename", {
       type: Sequelize.TEXT
-    });    
+    });
     await queryInterface.changeColumn("Groups", "groupname", {
       type: Sequelize.TEXT
     });
-     await queryInterface.changeColumn("Users", "username", {
+    await queryInterface.changeColumn("Users", "username", {
       type: Sequelize.TEXT
-    });       
+    });
     await queryInterface.changeColumn("Users", "email", {
       type: Sequelize.TEXT
     });
