ba3b4e3a415f544840c31518f840dcc744410fa1
Clare McLennan <57052+clare@users.noreply.github.com>
2020-06-08 14:25:43 +1200

    Fixed up travis.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+
+cd /
 
 echo "---- Starting Minio ----"
 ./minio server --address :9001 .data &> minio.log &
+
 
 echo "---- Starting PostgreSQL ----"
 service postgresql start
@@ -12,8 +16,9 @@ sudo -i -u postgres psql -c "CREATE DATABASE cacophonytest WITH OWNER test;"
 sudo -i -u postgres psql cacophonytest -c "CREATE EXTENSION postgis"
 sudo -i -u postgres psql cacophonytest -c "CREATE EXTENSION citext"
 
-
-echo "mc done"
+echo "---- Setting up Minio ----"
+./mc config host add myminio http://127.0.0.1:9001 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+./mc mb myminio/cacophony
 
 cd /app
 
@@ -21,18 +26,8 @@ echo "---- Migrating database ----"
 node_modules/.bin/sequelize db:migrate --config config/app_test_default.js
 sudo -i -u postgres psql cacophonytest -f /app/test/db-seed.sql
 
-cd ..
-
-echo "---- Setting up Minio ----"
-./mc config host add myminio http://127.0.0.1:9001 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
-./mc mb myminio/cacophony
-
-cd /app
-
-echo "---- Compiling TypeScript ----"
-node_modules/.bin/tsc .
-
-
 echo "alias psqltest='sudo -i -u postgres psql cacophonytest'" > ~/.bashrc
 
-./node_modules/.bin/tsc-watch --onSuccess "node Server.js --config=config/app_test_default.js"
+echo "---- Compiling typescript and starting module ----"
+./node_modules/.bin/tsc
+./node_modules/.bin/tsc-watch --noClear --onSuccess "node Server.js --config=config/app_test_default.js"
--- migrations/util/util.js
+++ migrations/util/util.js
@@ -16,95 +16,91 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-let __importDefault = (this && this.__importDefault) || function (mod) {
-  return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = __importDefault(require("sequelize"));
 
 function migrationAddBelongsTo(queryInterface, childTable, parentTable, opts) {
-    if (!opts) {
-        opts = {};
-    }
-    if (opts === "strict") {
-        opts = {
-            notNull: true,
-            cascade: true
-        };
-    }
-    let columnName = `${parentTable.substring(0, parentTable.length - 1)}Id`;
-    if (opts.name) {
-        columnName = `${opts.name}Id`;
-    }
-    const constraintName = `${childTable}_${columnName}_fkey`;
-    let deleteBehaviour = "SET NULL";
-    if (opts.cascade) {
-        deleteBehaviour = "CASCADE";
-    }
-    let columnNull = "";
-    if (opts.notNull) {
-        columnNull = "NOT NULL";
-    }
-    return new Promise(function (resolve, reject) {
-        queryInterface.sequelize
-            .query(`ALTER TABLE "${childTable}" ADD COLUMN "${columnName}" INTEGER ${columnNull};`)
-            .then(() => {
-            return queryInterface.sequelize.query(`ALTER TABLE "${childTable}" ADD CONSTRAINT "${constraintName}" FOREIGN KEY ("${columnName}") REFERENCES "${parentTable}" (id) ON DELETE ${deleteBehaviour} ON UPDATE CASCADE;`);
-        })
-            .then(() => resolve())
-            .catch(err => {
-            console.log(err);
-            reject(err);
-        });
-    });
+  if (!opts) {
+    opts = {};
+  }
+  if (opts === "strict") {
+    opts = {
+      notNull: true,
+      cascade: true
+    };
+  }
+  let columnName = `${parentTable.substring(0, parentTable.length - 1)}Id`;
+  if (opts.name) {
+    columnName = `${opts.name}Id`;
+  }
+  const constraintName = `${childTable}_${columnName}_fkey`;
+  let deleteBehaviour = "SET NULL";
+  if (opts.cascade) {
+    deleteBehaviour = "CASCADE";
+  }
+  let columnNull = "";
+  if (opts.notNull) {
+    columnNull = "NOT NULL";
+  }
+  return new Promise(function (resolve, reject) {
+    queryInterface.sequelize
+      .query(`ALTER TABLE "${childTable}" ADD COLUMN "${columnName}" INTEGER ${columnNull};`)
+      .then(() => {
+        return queryInterface.sequelize.query(`ALTER TABLE "${childTable}" ADD CONSTRAINT "${constraintName}" FOREIGN KEY ("${columnName}") REFERENCES "${parentTable}" (id) ON DELETE ${deleteBehaviour} ON UPDATE CASCADE;`);
+      })
+      .then(() => resolve())
+      .catch(err => {
+        console.log(err);
+        reject(err);
+      });
+  });
 }
 exports.migrationAddBelongsTo = migrationAddBelongsTo;
 function renameTableAndIdSeq(queryInterface, oldName, newName) {
-    return Promise.all([
-        queryInterface.sequelize.query(`ALTER TABLE "${oldName}" RENAME TO "${newName}";`),
-        queryInterface.sequelize.query(`ALTER TABLE "${oldName}_id_seq" RENAME TO "${newName}_id_seq";`)
-    ]);
+  return Promise.all([
+    queryInterface.sequelize.query(`ALTER TABLE "${oldName}" RENAME TO "${newName}";`),
+    queryInterface.sequelize.query(`ALTER TABLE "${oldName}_id_seq" RENAME TO "${newName}_id_seq";`)
+  ]);
 }
 exports.renameTableAndIdSeq = renameTableAndIdSeq;
 function migrationRemoveBelongsTo(queryInterface, childTable, parentTable, opts = {}) {
-    let columnName = `${parentTable.substring(0, parentTable.length - 1)}Id`;
-    if (opts.name) {
-        columnName = `${opts.name}Id`;
-    }
-    return queryInterface.sequelize.query(`ALTER TABLE "${childTable}" DROP COLUMN "${columnName}";`);
+  let columnName = `${parentTable.substring(0, parentTable.length - 1)}Id`;
+  if (opts.name) {
+    columnName = `${opts.name}Id`;
+  }
+  return queryInterface.sequelize.query(`ALTER TABLE "${childTable}" DROP COLUMN "${columnName}";`);
 }
 exports.migrationRemoveBelongsTo = migrationRemoveBelongsTo;
 function belongsToMany(queryInterface, viaTable, table1, table2) {
-    const columnName1 = `${table1.substring(0, table1.length - 1)}Id`;
-    const constraintName1 = `${viaTable}_${columnName1}_fkey`;
-    const columnName2 = `${table2.substring(0, table2.length - 1)}Id`;
-    const constraintName2 = `${viaTable}_${columnName2}_fkey`;
-    console.log("Adding belongs to many columns.");
-    return new Promise(function (resolve, reject) {
-        Promise.all([
-            queryInterface.sequelize.query(`ALTER TABLE "${viaTable}" ADD COLUMN "${columnName1}" INTEGER;`),
-            queryInterface.sequelize.query(`ALTER TABLE "${viaTable}" ADD COLUMN "${columnName2}" INTEGER;`)
-        ])
-            .then(() => {
-            console.log("Adding belongs to many constraint.");
-            return Promise.all([
-                queryInterface.sequelize.query(`ALTER TABLE "${viaTable}" ADD CONSTRAINT "${constraintName1}" FOREIGN KEY ("${columnName1}") REFERENCES "${table1}" (id) ON DELETE CASCADE ON UPDATE CASCADE;`),
-                queryInterface.sequelize.query(`ALTER TABLE "${viaTable}" ADD CONSTRAINT "${constraintName2}" FOREIGN KEY ("${columnName2}") REFERENCES "${table2}" (id) ON DELETE CASCADE ON UPDATE CASCADE;`)
-            ]);
-        })
-            .then(() => resolve())
-            .catch(err => reject(err));
-    });
+  const columnName1 = `${table1.substring(0, table1.length - 1)}Id`;
+  const constraintName1 = `${viaTable}_${columnName1}_fkey`;
+  const columnName2 = `${table2.substring(0, table2.length - 1)}Id`;
+  const constraintName2 = `${viaTable}_${columnName2}_fkey`;
+  console.log("Adding belongs to many columns.");
+  return new Promise(function (resolve, reject) {
+    Promise.all([
+      queryInterface.sequelize.query(`ALTER TABLE "${viaTable}" ADD COLUMN "${columnName1}" INTEGER;`),
+      queryInterface.sequelize.query(`ALTER TABLE "${viaTable}" ADD COLUMN "${columnName2}" INTEGER;`)
+    ])
+      .then(() => {
+        console.log("Adding belongs to many constraint.");
+        return Promise.all([
+          queryInterface.sequelize.query(`ALTER TABLE "${viaTable}" ADD CONSTRAINT "${constraintName1}" FOREIGN KEY ("${columnName1}") REFERENCES "${table1}" (id) ON DELETE CASCADE ON UPDATE CASCADE;`),
+          queryInterface.sequelize.query(`ALTER TABLE "${viaTable}" ADD CONSTRAINT "${constraintName2}" FOREIGN KEY ("${columnName2}") REFERENCES "${table2}" (id) ON DELETE CASCADE ON UPDATE CASCADE;`)
+        ]);
+      })
+      .then(() => resolve())
+      .catch(err => reject(err));
+  });
 }
 exports.belongsToMany = belongsToMany;
 function addSerial(queryInterface, tableName) {
-    return queryInterface.sequelize.query(`ALTER TABLE "${tableName}" ADD COLUMN id SERIAL PRIMARY KEY;`);
+  return queryInterface.sequelize.query(`ALTER TABLE "${tableName}" ADD COLUMN id SERIAL PRIMARY KEY;`);
 }
 exports.addSerial = addSerial;
 exports.default = {
-    migrationAddBelongsTo,
-    migrationRemoveBelongsTo,
-    belongsToMany,
-    addSerial,
-    renameTableAndIdSeq
+  migrationAddBelongsTo,
+  migrationRemoveBelongsTo,
+  belongsToMany,
+  addSerial,
+  renameTableAndIdSeq
 };
