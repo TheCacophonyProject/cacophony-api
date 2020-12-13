c4b8320d8a34f27a02922b7fa29e041eccebf331
Giampaolo <g.ferraro22@gmail.com>
2020-12-14 10:30:06 +1300

    add test for array of conditions

--- test/test_alerts.py
+++ test/test_alerts.py
@@ -12,18 +12,22 @@ class TestAlert:
         frank = helper.given_new_user(self, "frank")
         print("Franke cannot make an alert for the colonels device, without access")
         with pytest.raises(AuthorizationError):
-            alert_id = make_alert(frank, "Colonel always alert", "possum", colonel_device._id, frequency=0)
+            make_alert(frank, "Colonel always alert", "possum", colonel_device._id, frequency=0)
 
     def test_alert_bad_alert_condition(self, helper):
         colonel = helper.given_new_user(self, "colonel")
         colonel_group = helper.make_unique_group_name(self, "colonelGroup")
         colonel.create_group(colonel_group)
         colonel_device = helper.given_new_device(self, "colonel_device", colonel_group)
-        print("The colonel creates an alert for his device on possums 0 frequency, automatic tags")
 
         with pytest.raises(BadRequestError):
             colonel.create_alert(
                 "bad condition", [{"tag": "any", "automaticF": True}], colonel_device.get_id(), 0
+            )
+
+        with pytest.raises(BadRequestError):
+            colonel.create_alert(
+                "bad condition", {"tag": "any", "automatic": True}, colonel_device.get_id(), 0
             )
 
     def test_alert_automatic(self, helper):
