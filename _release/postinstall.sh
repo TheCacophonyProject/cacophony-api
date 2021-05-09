b98563dcb80dd4c9564cd776f13ad749a2465567
GP <g.ferraro22@gmail.com>
2021-05-10 09:27:00 +1200

    Fix multi (#376)
    * update multiparty
    
    * always mod lock file

--- _release/postinstall.sh
+++ _release/postinstall.sh
@@ -22,9 +22,8 @@ if [[ `stat -c '%U' node_modules` != "fullnoise" ]]; then
     rm -rf node_modules
     mkdir node_modules
     chown fullnoise:fullnoise node_modules
-    chown fullnoise:fullnoise package-lock.json
 fi
-
+chown fullnoise:fullnoise package-lock.json
 section "Installing dependencies"
 su fullnoise -s /bin/sh -c "npm install"
 
